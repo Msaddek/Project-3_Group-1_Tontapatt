537c21b41485ffcd6465d1ee7a6546b713bff341
Jessica <jessicaholmes34@hotmail.com>
2021-08-03 10:05:02 +0200

    fieldRegistration v1

--- TontapattWeb/src/main/java/fr/eql/ai109/controller/FieldManagedBean.java
+++ TontapattWeb/src/main/java/fr/eql/ai109/controller/FieldManagedBean.java
@@ -7,7 +7,12 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
 import fr.eql.ai109.ibusiness.FieldIBusiness;
+import fr.eql.ai109.tontapatt.entity.FenceHeight;
 import fr.eql.ai109.tontapatt.entity.Field;
+import fr.eql.ai109.tontapatt.entity.FlatnessPercentage;
+import fr.eql.ai109.tontapatt.entity.GrassHeight;
+import fr.eql.ai109.tontapatt.entity.VegetationType;
+import fr.eql.ai109.tontapatt.entity.ZipCodeCity;
 
 @ManagedBean(name = "mbField")
 @SessionScoped
@@ -20,7 +25,29 @@ public class FieldManagedBean implements Serializable {
 
 	@EJB
 	FieldIBusiness business;
+	
 	private Field field;
+	
+	private String name;
+	
+	private String address;
+	
+	private ZipCodeCity city;
+	
+	private Integer area;
+	
+	private String description;
+	
+	private String photo;
+	
+	private GrassHeight grassHeight;
+	
+	private FenceHeight fenceHeight;
+	
+	private FlatnessPercentage flatnessPercentage;
+	
+	private VegetationType vegetationType;
+		
 
 	public Field getField() {
 		return field;
@@ -29,5 +56,95 @@ public class FieldManagedBean implements Serializable {
 	public void setField(Field field) {
 		this.field = field;
 	}
+
+	public FieldIBusiness getBusiness() {
+		return business;
+	}
+
+	public void setBusiness(FieldIBusiness business) {
+		this.business = business;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public ZipCodeCity getCity() {
+		return city;
+	}
+
+	public void setCity(ZipCodeCity city) {
+		this.city = city;
+	}
+
+	public Integer getArea() {
+		return area;
+	}
+
+	public void setArea(Integer area) {
+		this.area = area;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+	public GrassHeight getGrassHeight() {
+		return grassHeight;
+	}
+
+	public void setGrassHeight(GrassHeight grassHeight) {
+		this.grassHeight = grassHeight;
+	}
+
+	public FenceHeight getFenceHeight() {
+		return fenceHeight;
+	}
+
+	public void setFenceHeight(FenceHeight fenceHeight) {
+		this.fenceHeight = fenceHeight;
+	}
+
+	public FlatnessPercentage getFlatnessPercentage() {
+		return flatnessPercentage;
+	}
+
+	public void setFlatnessPercentage(FlatnessPercentage flatnessPercentage) {
+		this.flatnessPercentage = flatnessPercentage;
+	}
+
+	public VegetationType getVegetationType() {
+		return vegetationType;
+	}
+
+	public void setVegetationType(VegetationType vegetationType) {
+		this.vegetationType = vegetationType;
+	}
+	
+	
 
 }
