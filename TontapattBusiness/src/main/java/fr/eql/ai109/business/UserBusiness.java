8b170d6e3549600b7344fa6373cf7ccfc35b42e1
Msaddek <basem.msaddek@gmail.com>
2021-08-03 19:45:11 +0200

    ajout fonction hashage du mot de passe (userBusiness), ajout de la condition si le user n'a pas de photo il prend la photo par default (userBusiness), mise en place de la page de l'utilisateur connecté (connectedPage et userHeader)

--- TontapattBusiness/src/main/java/fr/eql/ai109/business/UserBusiness.java
+++ TontapattBusiness/src/main/java/fr/eql/ai109/business/UserBusiness.java
@@ -1,5 +1,8 @@
 package fr.eql.ai109.business;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 import javax.ejb.EJB;
@@ -19,6 +22,10 @@ public class UserBusiness implements UserIBusiness {
 
 	@Override
 	public User add(User t) {
+		t.setPassword(hashage(t.getPassword()));
+		if (t.getPhoto() == null) {
+			t.setPhoto("resources/assets/img/user.png" );
+		}
 		return userIDAO.add(t);
 	}
 
@@ -53,7 +60,28 @@ public class UserBusiness implements UserIBusiness {
 
 	@Override
 	public User connection(String email, String password) {
+		password = hashage(password);
 		return userIDAO.authenticate(email, password);
+	}
+	
+	private String hashage(String str) {
+		
+        MessageDigest msg;
+		try {
+			msg = MessageDigest.getInstance("SHA-256");
+		    byte[] hash = msg.digest(str.getBytes(StandardCharsets.UTF_8));
+		    StringBuilder s = new StringBuilder();
+		    for (byte b : hash) {
+		    	s.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
+		    }
+		    str = s.toString();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return str;
+ 
 	}
 
 }
