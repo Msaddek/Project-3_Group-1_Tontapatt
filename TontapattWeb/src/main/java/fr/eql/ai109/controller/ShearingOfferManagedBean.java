76f15a73c24476723b75f1b79da28ecefa5c0520
Nicolas AYOUB <nicolas.ayoub5@outlook.com>
2021-08-05 15:01:30 +0200

    initiating connected user and post construct method

--- TontapattWeb/src/main/java/fr/eql/ai109/controller/ShearingOfferManagedBean.java
+++ TontapattWeb/src/main/java/fr/eql/ai109/controller/ShearingOfferManagedBean.java
@@ -1,13 +1,17 @@
 package fr.eql.ai109.controller;
 
 import java.io.Serializable;
+import java.util.Set;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 
 import fr.eql.ai109.ibusiness.ShearingOfferIBusiness;
 import fr.eql.ai109.tontapatt.entity.ShearingOffer;
+import fr.eql.ai109.tontapatt.entity.User;
 
 @ManagedBean(name = "mbShearingOffer")
 @SessionScoped
@@ -20,7 +24,28 @@ public class ShearingOfferManagedBean implements Serializable {
 
 	@EJB
 	ShearingOfferIBusiness business;
+	@ManagedProperty(value = "#{mbUser.user}")
+	private User connectedUser;
+
+	private Set<ShearingOffer> connectedUserOffers;
+
+	private Set<ShearingOffer> connectedUserExpiredOffers;
+
+	private Set<ShearingOffer> connectedUserInProgress;
+
 	private ShearingOffer shearingOffer;
+	
+	
+
+	@PostConstruct
+	public void init() {
+		connectedUserOffers = business
+				.getShearingOffersOfConnectedUser(connectedUser);
+		connectedUserExpiredOffers = business
+				.getExpiredShearingOffersOfConnectedUser(connectedUser);
+		connectedUserInProgress = business
+				.getInProgressShearingOffersOfConnectedUser(connectedUser);
+	}
 
 	public ShearingOffer getShearingOffer() {
 		return shearingOffer;
@@ -28,6 +53,40 @@ public class ShearingOfferManagedBean implements Serializable {
 
 	public void setShearingOffer(ShearingOffer shearingOffer) {
 		this.shearingOffer = shearingOffer;
+	}
+
+	public User getConnectedUser() {
+		return connectedUser;
+	}
+
+	public void setConnectedUser(User connectedUser) {
+		this.connectedUser = connectedUser;
+	}
+
+	public Set<ShearingOffer> getConnectedUserOffers() {
+		return connectedUserOffers;
+	}
+
+	public void setConnectedUserOffers(Set<ShearingOffer> connectedUserOffers) {
+		this.connectedUserOffers = connectedUserOffers;
+	}
+
+	public Set<ShearingOffer> getConnectedUserExpiredOffers() {
+		return connectedUserExpiredOffers;
+	}
+
+	public void setConnectedUserExpiredOffers(
+			Set<ShearingOffer> connectedUserExpiredOffers) {
+		this.connectedUserExpiredOffers = connectedUserExpiredOffers;
+	}
+
+	public Set<ShearingOffer> getConnectedUserInProgress() {
+		return connectedUserInProgress;
+	}
+
+	public void setConnectedUserInProgress(
+			Set<ShearingOffer> connectedUserInProgress) {
+		this.connectedUserInProgress = connectedUserInProgress;
 	}
 
 }
