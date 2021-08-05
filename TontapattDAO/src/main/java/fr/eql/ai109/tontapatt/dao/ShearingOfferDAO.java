09a3eba9588d76cd93f8b9f22c8ca0f30bd9ba0e
Frédéric Guyont <guyont.frederic@gmail.com>
2021-08-05 16:59:32 +0200

    add find method in ShearingOffer

--- TontapattDAO/src/main/java/fr/eql/ai109/tontapatt/dao/ShearingOfferDAO.java
+++ TontapattDAO/src/main/java/fr/eql/ai109/tontapatt/dao/ShearingOfferDAO.java
@@ -1,14 +1,27 @@
 package fr.eql.ai109.tontapatt.dao;
 
+import java.util.Set;
+
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
+import javax.persistence.Query;
 
 import fr.eql.ai109.tontapatt.entity.ShearingOffer;
+import fr.eql.ai109.tontapatt.entity.ShearingOfferPhoto;
+import fr.eql.ai109.tontapatt.entity.User;
 import fr.eql.ai109.tontapatt.idao.ShearingOfferIDAO;
 
 @Remote(ShearingOfferIDAO.class)
 @Stateless
 public class ShearingOfferDAO extends GenericDAO<ShearingOffer>
 		implements ShearingOfferIDAO {
-
+	
+	@Override
+	public Set<ShearingOffer> findShearingOffersByUser(User user) {
+		Set<ShearingOffer> offers = null;
+		Query query = em.createQuery("SELECT so FROM ShearingOffer so WHERE so.breeder=:breederParam ");
+		query.setParameter("breederParam", user);
+		offers = (Set<ShearingOffer>) query.getResultList();
+		return offers;
+	}
 }
