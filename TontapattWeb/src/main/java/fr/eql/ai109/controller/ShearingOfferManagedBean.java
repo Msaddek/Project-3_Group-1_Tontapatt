1fb3aadbab89076410f6bc545b6bf5fd966be010
Nicolas AYOUB <nicolas.ayoub5@outlook.com>
2021-08-05 16:07:58 +0200

    add offer creation method with offer properties

--- TontapattWeb/src/main/java/fr/eql/ai109/controller/ShearingOfferManagedBean.java
+++ TontapattWeb/src/main/java/fr/eql/ai109/controller/ShearingOfferManagedBean.java
@@ -1,6 +1,7 @@
 package fr.eql.ai109.controller;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.Set;
 
 import javax.annotation.PostConstruct;
@@ -10,8 +11,10 @@ import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 
 import fr.eql.ai109.ibusiness.ShearingOfferIBusiness;
+import fr.eql.ai109.tontapatt.entity.Race;
 import fr.eql.ai109.tontapatt.entity.ShearingOffer;
 import fr.eql.ai109.tontapatt.entity.User;
+import fr.eql.ai109.tontapatt.entity.ZipCodeCity;
 
 @ManagedBean(name = "mbShearingOffer")
 @SessionScoped
@@ -24,6 +27,7 @@ public class ShearingOfferManagedBean implements Serializable {
 
 	@EJB
 	ShearingOfferIBusiness business;
+	
 	@ManagedProperty(value = "#{mbUser.user}")
 	private User connectedUser;
 
@@ -35,7 +39,27 @@ public class ShearingOfferManagedBean implements Serializable {
 
 	private ShearingOffer shearingOffer;
 	
+	private String address;
 	
+	private Integer animalCount;
+	
+	private Double animalDailyPrice;
+	
+	private LocalDateTime creationDate;
+	
+	private String description;
+	
+	private LocalDateTime endDate;
+	
+	private Integer maxTravelDistance;
+	
+	private String name;
+	
+	private LocalDateTime startDate;
+	
+	private Race race;
+	
+	private ZipCodeCity city;
 
 	@PostConstruct
 	public void init() {
@@ -45,6 +69,23 @@ public class ShearingOfferManagedBean implements Serializable {
 				.getExpiredShearingOffersOfConnectedUser(connectedUser);
 		connectedUserInProgress = business
 				.getInProgressShearingOffersOfConnectedUser(connectedUser);
+	}
+	
+	public String createOffer() {
+		shearingOffer.setCreationDate(LocalDateTime.now());
+		shearingOffer.setAddress(address);
+		shearingOffer.setAnimalCount(animalCount);
+		shearingOffer.setAnimalDailyPrice(animalDailyPrice);
+		shearingOffer.setDescription(description);
+		shearingOffer.setEndDate(endDate);
+		shearingOffer.setMaxTravelDist(maxTravelDistance);
+		shearingOffer.setName(name);
+		shearingOffer.setStartDate(startDate);
+		shearingOffer.setBreeder(connectedUser);
+		shearingOffer.setRace(race);
+		shearingOffer.setZipCodeCity(city);
+		shearingOffer = business.add(shearingOffer);
+		return "/shearingOfferCreationDone.xhtml?faces-redirect=true";
 	}
 
 	public ShearingOffer getShearingOffer() {
@@ -87,6 +128,94 @@ public class ShearingOfferManagedBean implements Serializable {
 	public void setConnectedUserInProgress(
 			Set<ShearingOffer> connectedUserInProgress) {
 		this.connectedUserInProgress = connectedUserInProgress;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Integer getAnimalCount() {
+		return animalCount;
+	}
+
+	public void setAnimalCount(Integer animalCount) {
+		this.animalCount = animalCount;
+	}
+
+	public Double getAnimalDailyPrice() {
+		return animalDailyPrice;
+	}
+
+	public void setAnimalDailyPrice(Double animalDailyPrice) {
+		this.animalDailyPrice = animalDailyPrice;
+	}
+
+	public LocalDateTime getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(LocalDateTime creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDateTime getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDateTime endDate) {
+		this.endDate = endDate;
+	}
+
+	public Integer getMaxTravelDistance() {
+		return maxTravelDistance;
+	}
+
+	public void setMaxTravelDistance(Integer maxTravelDistance) {
+		this.maxTravelDistance = maxTravelDistance;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDateTime getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDateTime startDate) {
+		this.startDate = startDate;
+	}
+
+	public Race getRace() {
+		return race;
+	}
+
+	public void setRace(Race race) {
+		this.race = race;
+	}
+
+	public ZipCodeCity getCity() {
+		return city;
+	}
+
+	public void setCity(ZipCodeCity city) {
+		this.city = city;
 	}
 
 }
