7180ad4f1002cb10a22e4934926f0a1f0cdfb74d
Nicolas AYOUB <nicolas.ayoub5@outlook.com>
2021-08-05 16:47:00 +0200

    adding @Remote and @Stateless

--- TontapattDAO/src/main/java/fr/eql/ai109/tontapatt/dao/VegetationCompositionDAO.java
+++ TontapattDAO/src/main/java/fr/eql/ai109/tontapatt/dao/VegetationCompositionDAO.java
@@ -1,8 +1,14 @@
 package fr.eql.ai109.tontapatt.dao;
 
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+
 import fr.eql.ai109.tontapatt.entity.VegetationComposition;
 import fr.eql.ai109.tontapatt.idao.VegetationCompositionIDAO;
 
-public class VegetationCompositionDAO extends GenericDAO<VegetationComposition> implements VegetationCompositionIDAO{
+@Remote(VegetationCompositionIDAO.class)
+@Stateless
+public class VegetationCompositionDAO extends GenericDAO<VegetationComposition>
+		implements VegetationCompositionIDAO {
 
 }
