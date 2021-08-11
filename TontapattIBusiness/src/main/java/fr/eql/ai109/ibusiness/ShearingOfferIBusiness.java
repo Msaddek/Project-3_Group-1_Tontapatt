c93b3de2f3ffcacf30d38c990cd3b568be42903a
Frédéric Guyont <guyont.frederic@gmail.com>
2021-08-11 18:55:43 +0200

    import Field in ShearingOfferIbusiness

--- TontapattIBusiness/src/main/java/fr/eql/ai109/ibusiness/ShearingOfferIBusiness.java
+++ TontapattIBusiness/src/main/java/fr/eql/ai109/ibusiness/ShearingOfferIBusiness.java
@@ -2,6 +2,7 @@ package fr.eql.ai109.ibusiness;
 
 import java.util.Set;
 
+import fr.eql.ai109.tontapatt.entity.Field;
 import fr.eql.ai109.tontapatt.entity.ShearingOffer;
 import fr.eql.ai109.tontapatt.entity.User;
 
