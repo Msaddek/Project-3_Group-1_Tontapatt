18045ca4b646497e7d6417300a887d838573ca20
Jessica <jessicaholmes34@hotmail.com>
2021-08-12 17:16:03 +0200

    fieldWithdrawal popup

--- TontapattWeb/src/main/java/fr/eql/ai109/controller/FieldManagedBean.java
+++ TontapattWeb/src/main/java/fr/eql/ai109/controller/FieldManagedBean.java
@@ -106,6 +106,12 @@ public class FieldManagedBean implements Serializable {
 		return "/fieldUpdate.xhtml?faces-redirect=true";
 
 	}
+	
+	public String fieldDetailedView(Field field) {
+		this.field = field;
+		return "/fieldDetails.xhtml?faces-redirect=true";
+
+	}
 
 	public String createField() {
 		String forward = "/fieldParameters.xhtml?faces-redirect=true";
