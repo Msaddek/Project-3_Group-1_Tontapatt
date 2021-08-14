20c614e16e87e1b53c42b65b75686cb0da8773d8
Nicolas AYOUB <nicolas.ayoub5@outlook.com>
2021-08-14 14:25:56 +0200

    creating method that takes into account the species selected by the user for offer search

--- TontapattIBusiness/src/main/java/fr/eql/ai109/ibusiness/ShearingOfferIBusiness.java
+++ TontapattIBusiness/src/main/java/fr/eql/ai109/ibusiness/ShearingOfferIBusiness.java
@@ -5,13 +5,19 @@ import java.util.Set;
 
 import fr.eql.ai109.tontapatt.entity.Field;
 import fr.eql.ai109.tontapatt.entity.ShearingOffer;
+import fr.eql.ai109.tontapatt.entity.Species;
 import fr.eql.ai109.tontapatt.entity.User;
 
 public interface ShearingOfferIBusiness
 		extends GenericIBusiness<ShearingOffer> {
 
 	Set<ShearingOffer> searchOfferByFieldLocation(Field field,
-			LocalDate startDate, LocalDate endDate, Integer requiredAnimalCount);
+			LocalDate startDate, LocalDate endDate,
+			Integer requiredAnimalCount);
+
+	public Set<ShearingOffer> searchOfferByFieldLocationAndSpecies(Field field,
+			LocalDate serviceStartDate, LocalDate serviceEndDate,
+			Integer requiredAnimalCount, Species selectedSpecies);
 
 	Set<ShearingOffer> getShearingOffersOfConnectedUser(User user);
 
