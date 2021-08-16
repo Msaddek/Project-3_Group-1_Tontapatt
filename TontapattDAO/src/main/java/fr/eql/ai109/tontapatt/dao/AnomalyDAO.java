8a335549622b7af845130c6ff92932c4e97bda3a
hajjoujti <HAJJ@192.168.1.18>
2021-08-16 11:20:00 +0200

    creating method getAllByService for anomalies and linking add and update with DAO

--- TontapattDAO/src/main/java/fr/eql/ai109/tontapatt/dao/AnomalyDAO.java
+++ TontapattDAO/src/main/java/fr/eql/ai109/tontapatt/dao/AnomalyDAO.java
@@ -1,14 +1,31 @@
 package fr.eql.ai109.tontapatt.dao;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
 
 import fr.eql.ai109.tontapatt.entity.Anomaly;
+import fr.eql.ai109.tontapatt.entity.Service;
 import fr.eql.ai109.tontapatt.idao.AnomalyIDAO;
 
 @Remote(AnomalyIDAO.class)
 @Stateless
 public class AnomalyDAO extends GenericDAO<Anomaly>
 		implements AnomalyIDAO {
+
+	@Override
+	public Set<Anomaly> getAllByService(Service selectedService) {
+		Set<Anomaly> anomalies = null;
+		try {
+			anomalies = new HashSet<>(em.createQuery(
+					"SELECT a FROM Anomaly a WHERE a.service=:serviceParam")
+					.setParameter("serviceParam", selectedService).getResultList());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return anomalies;
+	}
 
 }
