cd9f7f3555011b9ab985134eda3f1bfbc4b20658
Msaddek <basem.msaddek@gmail.com>
2021-08-17 15:06:56 +0200

    Ajout de l'icon du site

--- TontapattWeb/src/main/java/fr/eql/ai109/controller/FieldManagedBean.java
+++ TontapattWeb/src/main/java/fr/eql/ai109/controller/FieldManagedBean.java
@@ -96,6 +96,7 @@ public class FieldManagedBean implements Serializable {
 				.getFieldsOfConnectedUser(connectedUser);
 		photos = new HashSet<>();
 		vegetationCompositions = new HashSet<>();
+		System.out.println(connectedUser);
 	}
 
 	public String fieldDetails() {
